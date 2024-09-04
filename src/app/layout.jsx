e954162833b046d7ea59523cefd30319d38b5e05
hung <hung991703@gmail.com>
2024-09-04 19:27:38 +0700

    add fontawesome

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css"
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script src="https://kit.fontawesome.com/e3374afba4.js" crossOrigin="anonymous" />
     </html>
   );
 }
