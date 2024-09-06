f01cd8665df620014e0df2824c4f8b364daa4aeb
hung <hung991703@gmail.com>
2024-09-06 10:24:06 +0700

    update Services, Technologies

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,65 +1,88 @@
 "use client";
+import React from "react";
 import { useEffect, useRef, useState } from "react";
-import styles from "./styles.module.css"
-
+import { useTranslations } from "next-intl";
+import styles from "./styles.module.css";
 
 export default function Services() {
-    const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const t = useTranslations("Services");
 
-    const services = [
-        {
-            icon: "./images/services/web-Custom.png",
-            title: "WEB DEVELOPMENT",
-            description: "We design, build, and maintain websites and web applications for clients, providing front-end and back-end development, hosting, optimization, and security services. Our web development services aim to help clients establish a strong online presence to support their business goals."
-        },
-        {
-            icon: "./images/services/mobile-Custom.png",
-            title: "MOBILE DEVELOPMENT",
-            description: "We design, build, and maintain mobile applications for iOS and Android platforms. Our services include features like push notifications, social media integration, and in-app purchases. We also offer optimization and security to ensure a seamless and secure user experience for client's mobile presence."
-        },
-        {
-            icon: "./images/services/building-Custom.png",
-            title: "ENTERPRISE DEVELOPMENT",
-            description: "We build and maintain large-scale, mission-critical software applications for businesses, providing scalability, security, and business process automation. Our enterprise development services aim to help clients achieve their business objectives through cloud deployment, integration with other enterprise systems, and ongoing support and maintenance."
-        },
-        {
-            icon: "./images/services/outsource-Custom.png",
-            title: "OUTSOURCING DEVELOPMENT",
-            description: "We build and maintain remote software applications for businesses, adhering to client's requirements and deadlines. Our outsourcing services help clients reduce costs, increase efficiency, and achieve their business objectives."
-        },
-    ];
+  const services = [
+    {
+      icon: "./images/services/web-Custom.png",
+      title: "WEB DEVELOPMENT",
+      description:
+        "We design, build, and maintain websites and web applications for clients, providing front-end and back-end development, hosting, optimization, and security services. Our web development services aim to help clients establish a strong online presence to support their business goals.",
+      index: 0,
+    },
+    {
+      icon: "./images/services/mobile-Custom.png",
+      title: "MOBILE DEVELOPMENT",
+      description:
+        "We design, build, and maintain mobile applications for iOS and Android platforms. Our services include features like push notifications, social media integration, and in-app purchases. We also offer optimization and security to ensure a seamless and secure user experience for client's mobile presence.",
+      index: 1,
+    },
+    {
+      icon: "./images/services/building-Custom.png",
+      title: "ENTERPRISE DEVELOPMENT",
+      description:
+        "We build and maintain large-scale, mission-critical software applications for businesses, providing scalability, security, and business process automation. Our enterprise development services aim to help clients achieve their business objectives through cloud deployment, integration with other enterprise systems, and ongoing support and maintenance.",
+      index: 2,
+    },
+    {
+      icon: "./images/services/outsource-Custom.png",
+      title: "OUTSOURCING DEVELOPMENT",
+      description:
+        "We build and maintain remote software applications for businesses, adhering to client's requirements and deadlines. Our outsourcing services help clients reduce costs, increase efficiency, and achieve their business objectives.",
+      index: 3,
+    },
+  ];
 
-    return (
-        <section id="services" className={styles.services_wrapper}>
-            <div className={`container my-5`}>
-                <h2 className={`text-center ${styles.section_subtitle}`}>#WHAT WE DO</h2>
-                <h1 className={`text-center mb-4 ${styles.section_title}`}>SERVICES</h1>
-                <p className={`text-center mb-5 ${styles.section_desc}`}>
-                    We offer web, mobile, enterprise, and outsourcing development services to help clients achieve their business goals.
-                </p>
+  return (
+    <section id="services" className={styles.services_wrapper}>
+      <div className={`container my-5`}>
+        <h2 className={`text-center ${styles.section_subtitle}`}>
+          #{t("subtitle")}
+        </h2>
+        <h1 className={`text-center mb-4 ${styles.section_title}`}>
+          {t("title")}
+        </h1>
+        <p className={`text-center mb-5 ${styles.section_desc}`}>
+          {t("description.0")}
+          <br />
+          {t("description.1")}
+        </p>
 
-                <div className="row">
-                    {services.map((service, index) => (
-                        <div className="col-md-12 mb-4" key={index}>
-                            <div
-                                className={`${styles.service} border-0`}
-                                onMouseEnter={() => setHoveredIndex(index)}
-                                onMouseLeave={() => setHoveredIndex(null)}
-                            >
-                                <div className={styles.service_body}>
-                                    <div className={styles.service_icon_wrapper}>
-                                        <img src={service.icon}/>
-                                    </div>
-                                    <div className={styles.service_content}>
-                                        <h4 className={styles.service_title}>{service.title}</h4>
-                                        <p className={styles.service_text}>{service.description}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+        <div className="row">
+          {services.map((service, index) => (
+            <div className="col-md-12 mb-4" key={index}>
+              <div
+                className={`${styles.service} border-0`}
+                onMouseEnter={() => setHoveredIndex(index)}
+                onMouseLeave={() => setHoveredIndex(null)}
+              >
+                <div className={styles.service_body}>
+                  <div className={styles.service_icon_wrapper}>
+                    <img src={service.icon} />
+                  </div>
+                  <div className={styles.service_content}>
+                    <h4
+                      className={styles.service_title}
+                      style={{ textTransform: "uppercase" }}
+                    >
+                      {t(`items.${service.index}.title`)}
+                    </h4>
+                    <p className={styles.service_text}>
+                      {t(`items.${service.index}.description`)}
+                    </p>
+                  </div>
                 </div>
+              </div>
             </div>
-        </section>
-    );
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
