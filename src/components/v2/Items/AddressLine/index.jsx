49f4b66d830ed9f645be70483d21dc880048ccdd
hung <hung991703@gmail.com>
2024-09-07 12:44:28 +0700

    fix AddressLine

--- src/components/v2/Items/AddressLine/index.jsx
+++ src/components/v2/Items/AddressLine/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./styles.module.css"
+import classNames from "classnames";
 
-export default function AddressLine({ icon, info }) {
+export default function AddressLine({ icon = "fa-phone", info }) {
   return (
-    <div className={`${styles.root}`}>
-      <i className="fa fa-phone"></i>
+    <div className={classNames(styles.root)}>
+      <i className={classNames("fa", icon)}></i>
       <span style={{marginRight: "1em"}}></span>
       {info}
     </div>
