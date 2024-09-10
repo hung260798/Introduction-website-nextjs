27d33bc02ad3df9c53ece6b22add93859239b70c
hung <hung991703@gmail.com>
2024-09-10 14:39:17 +0700

    change

--- src/components/v2/Sections/Home/index.jsx
+++ src/components/v2/Sections/Home/index.jsx
@@ -7,20 +7,20 @@ import { useTranslations } from "next-intl";
 
 export default function Home() {
   const sectionRef = useNavigation();
-  const t = useTranslations("HomePage")
+  const t = useTranslations("HomePage");
   return (
     <div className={styles.root} id="home" ref={sectionRef}>
       <div className={classNames(styles.contentWrap)}>
         <div className={classNames(styles.content)}>
-          <div className={styles.title}>
-            BKHIGHTECH
-          </div>
-          <div className={styles.subtitle}>
-            {t("title")}
-          </div>
-          <div>
-            <button className={styles.btn}>our services</button>
-            <button className={styles.btn2}>get started</button>
+          <div className={styles.title}>BKHIGHTECH</div>
+          <div className={styles.subtitle}>{t("title")}</div>
+          <div className={styles.buttonsDiv}>
+            <div>
+              <button className={styles.btn}>our services</button>
+            </div>
+            <div>
+              <button className={styles.btn2}>get started</button>
+            </div>
           </div>
         </div>
       </div>
