c6035afb44fef789ead6f03ef81a337b328577a4
hung <hung991703@gmail.com>
2024-09-11 23:44:41 +0700

    update NavBar, Home

--- src/components/v2/Sections/Home/index.jsx
+++ src/components/v2/Sections/Home/index.jsx
@@ -1,27 +1,55 @@
-"use client"
+"use client";
 
 import classNames from "classnames";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import useNavigation from "@/hooks/navigationHook";
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 export default function Home() {
   const sectionRef = useNavigation();
   const t = useTranslations("HomePage");
+  const backgroundsImages = [
+    "/images/home/home1-medium.jpg",
+    "/images/home/home3-medium.jpg",
+  ];
+  const [background, setBackground] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setBackground((idx) => idx + 1);
+    }, 5000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <div className={styles.root} id="home" ref={sectionRef}>
-      <div className={classNames(styles.contentWrap)}>
+    <div
+      className={styles.root}
+      id="home"
+      style={{
+        backgroundImage: `url("${
+          backgroundsImages[background % backgroundsImages.length]
+        }")`,
+      }}
+      ref={sectionRef}
+    >
+      <div className={classNames(styles.contentWrapper)}>
         <div className={classNames(styles.content)}>
           <div className={styles.title}>BKHIGHTECH</div>
           <div className={styles.subtitle}>{t("title")}</div>
           <div className={styles.buttonsDiv}>
             <div>
-              <button className={styles.btn}>our services</button>
+              <Link className={styles.btnBlank} href={"/#services"}>
+                our services
+              </Link>
             </div>
             <div>
-              <button className={styles.btn2}>get started</button>
+              <Link className={styles.btnBlank} href={"/#contact"}>
+                get started
+              </Link>
             </div>
           </div>
         </div>
